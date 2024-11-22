310709ec60a9d58dfaddcedb2908eaf4d4b125db
Suraj chauhan <surajssc1232@gmail.com>
2024-11-22 14:21:24 +0530

    PWA for android

--- script.js
+++ script.js
@@ -367,3 +367,16 @@ function updateTime() {
 // Initial update and start interval
 updateTime();
 setInterval(updateTime, 1000);
+
+// Register Service Worker for PWA
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('sw.js')
+            .then((registration) => {
+                console.log('ServiceWorker registration successful');
+            })
+            .catch((err) => {
+                console.log('ServiceWorker registration failed: ', err);
+            });
+    });
+}
